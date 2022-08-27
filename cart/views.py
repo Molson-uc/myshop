af222f54ff2408b2b1090bf8cbfe4f099d6a91fa
Molson <molson.and@gmail.com>
2022-08-27 15:28:56 +0200

    fix remove item from cart

--- cart/views.py
+++ cart/views.py
@@ -14,8 +14,6 @@ def cart_add(request, product_id):
     form = CartAddProductForm(request.POST)
     if form.is_valid():
         cd = form.cleaned_data
-        print(cd)
-        print(cd.values)
         cart.add(
             product=product, quantity=cd["quantity"], update_quantity=cd["override"]
         )
@@ -25,7 +23,9 @@ def cart_add(request, product_id):
 def cart_detail(request):
     cart = Cart(request)
     for item in cart:
-        item["update_quantity_form"] = CartAddProductForm(initial={"quantity":item["quantity"], "override":True})
+        item["update_quantity_form"] = CartAddProductForm(
+            initial={"quantity": item["quantity"], "override": True}
+        )
     return render(request, "cart/detail.html", {"cart": cart})
 
 
@@ -33,5 +33,6 @@ def cart_detail(request):
 def cart_remove(request, product_id):
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
+    print("remove")
     cart.remove(product)
     return redirect("cart:cart_detail")
