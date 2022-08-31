4781f357dca292fbf1145e3ee4203d82a4d1ae6f
Molson <molson.and@gmail.com>
2022-08-31 18:12:16 +0200

    add celery

---
+++ myshop/celery.py
@@ -0,0 +1,9 @@
+from argparse import Namespace
+import os
+from celery import Celery
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myshop.settings")
+
+app=Celery("myshop")
+app.config_from_object("django.conf:settings",  namespace="CELERY")
+app.autodiscover_tasks()
